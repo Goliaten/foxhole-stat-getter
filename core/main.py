8826c4bda2a556ad1536e1b069e9343e4d029d1f
Goliaten <ognikat4@gmail.com>
2025-03-06 23:17:37 +0100

    entry script and basic main

--- core/main.py
+++ core/main.py
@@ -1,4 +1,18 @@
-def main(): ...
+from sys import path
+
+path.append("..")
+
+from core.runners.Startup import Startup
+from core.runners.Scraper import Scraper
+from core.runners.Processor import Processor
+from core.system import setup
+
+
+def main():
+    setup()
+    Startup().run()
+    Scraper().run()
+    Processor().run()
 
 
 # startup - show info to user, what's gonna happen
