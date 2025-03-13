eebaf4c9b7503661458f623ed08a8a2859d19e8e
Goliaten <ognikat4@gmail.com>
2025-03-13 23:19:31 +0100

    config prettifying

--- core/config.py
+++ core/config.py
@@ -1,10 +1,11 @@
-START_DELAY = 3  # in seconds
-ACTIVITY_TIME_OFFSET = 0.5  # in seconds
-
+# ========================
+# General app config
+# ========================
 APP_NAME = "foxhole-stat-getter"
 
-
+# ========================
 # Logging configuration
+# ========================
 LOG_DIR = "logs"  # Directory to store log files
 LOG_PREFIX = "app"  # Prefix for log filenames
 LOG_EXTENSION = ".log"  # Extension for log files
@@ -15,6 +16,34 @@ LOG_VERBOSITY = (
 )
 LOG_STACK_LEVEL = 3
 
+# ========================
+# Startup class config
+# ========================
 STARTUP_RUN = False
+START_DELAY = 3  # in seconds
+ACTIVITY_TIME_OFFSET = 0.5  # in seconds
+
+# ========================
+# Scraper class config
+# ========================
 SCRAPER_RUN = True
+
+# ========================
+# Processor class config
+# ========================
 PROCESSOR_RUN = True
+
+# ========================
+# MouseMover class config
+# ========================
+MM_TYPEWRITE_INTERVAL = 0.1
+# Which MouseMover derived class should be used.
+# Options are:
+#   1 - PyAutoGui based MouseMover
+MM_CHOSEN_CLASS = 1
+
+# ========================
+# pyautogui config
+# ========================
+PAG_FAILSAFE = True
+PAG_PAUSE = 0
